10567f7b3557e0d76b6f227a290eb0e1a517611b
Muhammad Fahad Habib <41792311+FahdHabib@users.noreply.github.com>
2022-10-25 16:16:18 +0500

    Addition of parsers and wrappers, related to Act Report

---
+++ parser_activation_report_pta.py
@@ -0,0 +1,61 @@
+##### ACTIVATION REPORT PTA Parser ####
+
+#### Input Parameters ####
+# 1--> pattern
+# 2--> input directory path
+# 3--> Output directory path
+##########################
+import sys
+import os
+import glob
+
+# making global output file variable
+output = ''
+
+def processor(files_list, input_directory, output_directory):
+
+        global output
+
+        for file in files_list:
+                #open input, output files
+                data = open(input_directory + '/' + file, 'r', encoding="utf8", errors = 'ignore')
+                output = open(output_directory + '/' + file, 'w')
+                header = data.readline()
+                header = header.strip('\n')
+                header = header.split(',')
+                
+                #write header to output file
+                output.write(','.join(header))
+                output.write('\n')
+              
+                #iterate over each row in data
+                for row in data:
+                    fields = row.split(',')
+                    
+                    new_row = [x.strip('\n') for x in fields]
+
+                    new_row[29:] = [' '.join(new_row[29:])]
+                    
+                    if len(new_row) == 30:
+                        output.write(','.join(new_row))
+                        output.write('\n')
+                                                  
+                                
+                output.close()
+                data.close()
+
+
+def main():
+
+        length = len(sys.argv)
+        pattr=sys.argv[1]
+        input_directory = sys.argv[2]
+        output_directory = sys.argv[3]
+        files_list = glob.glob(input_directory + '/' + pattr)
+
+        files_list = [ file.split('/')[-1] for file in files_list ]
+
+        processor(files_list, input_directory, output_directory)
+
+if __name__ == "__main__":
+    main()
