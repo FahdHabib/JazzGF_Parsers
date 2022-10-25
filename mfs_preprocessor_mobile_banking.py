ebdb83e3f298b12d8dde405f8f7ba3bc52db8a50
Muhammad Fahad Habib <41792311+FahdHabib@users.noreply.github.com>
2022-10-25 15:47:22 +0500

    Added parsers and shell wrappers, related to MFS.

---
+++ mfs_preprocessor_mobile_banking.py
@@ -0,0 +1,50 @@
+##### MFS Pre Processor ####
+
+#### Input Parameters ####
+# 1--> pattern
+# 2--> input directory path
+# 3--> Output directory path
+##########################
+import sys
+import os
+import glob
+
+# making global output file variable
+output = ''
+
+def processor(files_list, input_directory, output_directory):
+
+	global output
+
+	for file in files_list:
+		if(os.path.getsize(input_directory + '/' + file) > 0):
+			data = open(input_directory + '/' + file, 'r')
+			output = open(output_directory + '/' + file, 'w')
+			for row in data:
+				outdata = row.strip('\n')
+				outdata = outdata.split(',')
+				if len(outdata) > 59:
+					outdata = outdata[:60]
+					output.write(','.join(outdata))
+					output.write('\n')
+
+
+			output.close()
+			data.close()
+
+	
+def main():
+
+	length = len(sys.argv)
+	pattr=sys.argv[1]
+	input_directory = sys.argv[2]
+	output_directory = sys.argv[3]
+	files_list = glob.glob(input_directory + '/' + pattr)
+
+	files_list = [ file.split('/')[-1] for file in files_list ]
+
+	processor(files_list, input_directory, output_directory)
+
+if __name__ == "__main__":
+    main()
+
